3efb7cf5e6bb1ca8a6f8211b522167a3bdd0936f
kiwisincebirth <mpruden@iMac-i5.local>
2014-07-30 21:23:27 +1000

    Documentation

--- libraries/FSM/FiniteStateMachine.h
+++ libraries/FSM/FiniteStateMachine.h
@@ -1,27 +1,12 @@
 /*
 ||
 || @file FiniteStateMachine.h
-|| @version 1.8
+|| @version 2.1 - by Mark Pruden
 || @author Alexander Brevig
 || @contact alexanderbrevig@gmail.com
 ||
 || @description
 || | Provide an easy way of making finite state machines
-|| | 
-|| | Version 1.8 introduces these changes :-
-|| | * Added Comparison Operator for State object allowing for direct == comparison
-|| | * Added a FSM property LastState (getLastState) to track prior state state, 
-|| |   typically used during an Enter callback function for conditional logic.
-|| | * Added a method to get NextState, typically called from an Exit callback function
-|| |   to allow for conditional functionality based on next state
-|| | 
-|| | Internal Changes include :-
-|| | * Definition of callback function is now as typedefs
-|| | * Changed the reset of "Time In Current State" counter to coincide with 
-|| |   calling the Enter callback Function. Previously it was being reset before
-|| |   the exit callback function was being called
-|| | * Constructors of State, now use proper C++ initialisation.
-|| | 
 || #
 ||
 || @license
@@ -39,23 +24,6 @@
 || | License along with this library; if not, write to the Free Software
 || | Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 || #
----------------------( COPY )---------------------
-This libraries .h file has been updated by Terry King to make it compatible with Ardino 1.0x 
-
-(Example 1.03) and also earlier versions like 0023
-
-#include "WProgram.h"  
-...has been replaced by:
-
-#if defined(ARDUINO) && ARDUINO >= 100
-#include "Arduino.h"
-#else
-#include "WProgram.h"
-#endif
-
-terry@yourduino.com
-02/07/2012
------------------( END COPY )----------------------
 ||
 */
 
@@ -76,15 +44,6 @@ terry@yourduino.com
 
 //forward declarations
 class State;
-
-// definitions of callback for Enter and Exit, the parameter is 
-// the state we transition away from, or state going to.
-
-//typedef void (*fsmTransCallback)(State *state);
-
-// definitions of callback for Update, milliseconds in the state
-//
-typedef void (*fsmUpdateCallback)(unsigned long milliseconds);
 
 // definitions of callback for Update, milliseconds in the state
 
@@ -141,11 +100,13 @@ class FiniteStateMachine {
 
 /*
 || @changelog
-|| | 1.8 2013-10-10 - Mark Pruden : Callback enhancements, see above.
-|| | 1.7 2010-03-08- Alexander Brevig : Fixed a bug, constructor ran update, thanks to René Pressé
-|| | 1.6 2010-03-08- Alexander Brevig : Added timeInCurrentState() , requested by sendhb
-|| | 1.5 2009-11-29- Alexander Brevig : Fixed a bug, introduced by the below fix, thanks to Jon Hylands again...
-|| | 1.4 2009-11-29- Alexander Brevig : Fixed a bug, enter gets triggered on the first state. Big thanks to Jon Hylands who pointed this out.
+|| | 2.1 2014-07-30 - Mark Pruden : See LINK: https://github.com/kiwisincebirth/Arduino/tree/master/libraries/FSM
+|| | 2.0 2013-02-08 - LINK: Versions fixed for Arduino 1.0x Terry King http://arduino-info.wikispaces.com/HAL-LibrariesUpdates
+|| | 1.8 2010-02-04 - Fixed a bug, missing return type, thanks to Rick Howard
+|| | 1.7 2010-03-08 - Alexander Brevig : Fixed a bug, constructor ran update, thanks to René Pressé
+|| | 1.6 2010-03-08 - Alexander Brevig : Added timeInCurrentState() , requested by sendhb
+|| | 1.5 2009-11-29 - Alexander Brevig : Fixed a bug, introduced by the below fix, thanks to Jon Hylands again...
+|| | 1.4 2009-11-29 - Alexander Brevig : Fixed a bug, enter gets triggered on the first state. Big thanks to Jon Hylands who pointed this out.
 || | 1.3 2009-11-01 - Alexander Brevig : Added getCurrentState : &State
 || | 1.3 2009-11-01 - Alexander Brevig : Added isInState : boolean, requested by Henry Herman 
 || | 1.2 2009-05-18 - Alexander Brevig : enter and exit bug fix
---
+++ libraries/FSM/README.md
@@ -0,0 +1,39 @@
+Finite State Machine Arduino Library
+====================================
+
+A FSM serves as a manager that organizes a set of states, or behaviors. It manages the transition between states, and the state itself.
+
+The transitions might be caused by the user, a wall or something other that is external, and they might be caused by some internal logic. Transitions either happen immediately, or they are deferred to the next update. The latter is the most common. It's used because that way you'll know that all code related to the current state, will have executed on the same state.
+
+See
+===
+http://playground.arduino.cc/code/FiniteStateMachine
+
+Version History
+===============
+
+2.1 2014-07-30 Introduces these changes :-
+
+ * Added Comparison Operator for State object allowing for direct == comparison
+ * Added a FSM property LastState (getLastState) to track prior state state, 
+   typically used during an Enter callback function for conditional logic.
+ * Added a method to get NextState, typically called from an Exit callback function
+   to allow for conditional functionality based on next state
+ 
+ Internal Changes include :-
+ * Definition of callback function is now as typedefs
+ * Changed the reset of "Time In Current State" counter to coincide with 
+   calling the Enter callback Function. Previously it was being reset before
+   the exit callback function was being called
+ * Constructors of State, now use proper C++ initialisation.
+
+2.0 2013-02-08: LINK: Versions fixed for Arduino 1.0x Terry King http://arduino-info.wikispaces.com/HAL-LibrariesUpdates
+1.8 2010-02-04: Fixed a bug, missing return type, thanks to Rick Howard
+1.7 2010-03-08: Fixed a bug, constructor ran update, thanks to René Pressé
+1.6 2010-03-08: Added timeInCurrentState() , requested by sendhb
+1.5 2009-11-29: Fixed a bug that was introduced by the last fix, thanks to Jon Hylands again
+1.4 2009-11-29: The first state now calls it's enter(), thank you Jon Hylands for bringing this bug to my attention.
+1.3 2009-11-01: Added getCurrentState and isInState, requested by Henry Herman 
+1.2 2009-05-18: Enter and exit bug fix
+1.1 2009-05-18: Added support for cascaded calls
+1.0 2009-04-11: Beta
